dc5f17a862dd55f02c2e1ca08dbedd16f1c43aba
pranav reddy <saipranav919@gmail.com>
2022-01-03 21:19:27 +0530

    add ticket feature implemented

--- ticketManagementSystem/script.js
+++ ticketManagementSystem/script.js
@@ -1,5 +1,10 @@
-let addBtn = document.querySelector(".fa-plus-circle");
+const addBtn = document.querySelector(".fa-plus-circle");
 const ticketCreator = document.querySelector(".ticket-creator");
+const inputBox = document.querySelector(".input-box");
+const filters = document.querySelectorAll(".filter");
+const mainContainer = document.querySelector(".main-container");
+const colors = ["blue","green","pink","black"];
+let cColor = colors[colors.length-1];
 let addFlag = false;
 
 // to make ticketcreator appear and disappear
@@ -11,4 +16,48 @@ addBtn.addEventListener("click", function(){
     }
     addFlag = !addFlag;
 });
+
+//color container in ticket creator animation and storing cplor value
+for(let i=0;i<filters.length;i++)
+{
+    filters[i].addEventListener("click",function(){
+
+        filters.forEach(function(colorEl){
+            colorEl.classList.remove("border");
+        })
+        filters[i].classList.add("border");
+        cColor = filters[i].classList[1];
+    })
+}
+
+//when clicked enter calls a fucntion to make a new ticket
+inputBox.addEventListener("keydown",function(e){
+    if(e.key=="Enter")
+    {
+        
+        let inputText = inputBox.value;
+        console.log(inputText,cColor)
+        
+        createTicket(inputText,cColor);
+
+        ticketCreator.style.display = "none";
+        inputBox.value = "";
+        addFlag = false;
+
+        cColor = colors[colors.length-1];
+    }
+})
+
+function createTicket(inputText,cColor)
+{
+    let ticket = document.createElement("div");
+    ticket.setAttribute("class","ticket-container");
+    ticket.innerHTML = 
+            `<div class="ticket-color ${cColor}"></div>
+            <div class="ticket-content">
+                <h3 class="ticket-id">#sampleId</h3>
+                <p class="ticket-desc">${inputText}</p>
+            </div>`;
+    mainContainer.appendChild(ticket);
+}
 
