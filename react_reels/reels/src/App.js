e6f741e3adaf926afa356994dbad0dbbfefc7997
pranav reddy <saipranav919@gmail.com>
2022-02-19 17:17:23 +0530

    login ui implemented

--- react_reels/reels/src/App.js
+++ react_reels/reels/src/App.js
@@ -1,39 +1,18 @@
 import './App.css';
 import React,{useContext,useEffect} from 'react';
 import {Switch,Route,BrowserRouter as Router,Redirect, BrowserRouter} from "react-router-dom";
-import Feed from './components/Feed';
-import Login from './components/Login';
-import Signup from './components/Signup';
-// import AuthContext from './'
+import Login from './components/Login.js';
+import Signup from './components/Signup.js';
 
 function App(){
   return(
-    // <AuthProvider>
-    //   <Router>
-    //     <Switch>
-    //       <Route path="/login" component={Login}></Route>
-    //       <Route path="/signup" component={SignUp}></Route>
-    //       <PrivateRoute path="/" exact component={Feed}></PrivateRoute>
-    //     </Switch>
-    //   </Router>
-    // </AuthProvider>
     <BrowserRouter>
-        <Signup />
+    <Switch>
+        <Route to='/login' component={Login} />
+        <Route to='/signup' component={Signup} />
+      </Switch>
     </BrowserRouter>
     
-  )
+  );
 }
-
-// function PrivateRoute(parentProps){
-//   let {currentUser} = useContext(AuthContext);
-//   let Component = parentProps.component;
-//   return(
-//     <Route {...parentProps} render={
-//       (parentProps)=>{
-//         currentUser!=null ? <Component {...parentProps}></Component> : <Redirect to="/login"></Redirect>
-//       }
-//     }></Route>  
-//   )
-
-// }
 export default App;
--- react_reels/reels/src/components/Login.js
+++ react_reels/reels/src/components/Login.js
@@ -1,45 +1,102 @@
-// import React from 'react';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import logo from "../assets/Instagram-Logo.png";
+import "./Login.css";
+import Alert from "@mui/material/Alert";
+import TextField from "@mui/material/TextField";
+import iphone7 from "../assets/iphone7.jpg";
+// import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import { Link } from "react-router-dom";
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext,Image } from 'pure-react-carousel';
+import img1 from "../assets/img1.jpg"
+import img2 from "../assets/img2.jpg"
+import img3 from "../assets/img3.jpg"
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
-// let login = useContext(AuthContext);
-// export default function Login() {
-//   const [error,setError] = useState(false);
-//   const [email,setEmail] = useState("");
-//   const [user,setUser] = useState(null);
-//   const [password,setPassword] = useState("");
-//   const [loader,setLoader] = useState(false);
-  
-//   const handleEmailInput = (e)=>{
-//     setEmail(e.target.value);
-//   }
-
-//   const handlePasswordInput = (e)=>{
-//     setPassword(e.target.value);
-//   }
-
-//   const handleSubmit = async (e)=>{
-//     e.preventDefault();
-//     try{
-//       setLoader(true);
-//       let res = await Login(email,password);
-//       setLoader(false);
-//       props.history.push("/");
-//     }
-//     catch(err){
-//       setError(true);
-//       setLoader(true);
-
-//       setEmail("");
-//       setPassword("");
-//     }
-//   }
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit}>
-//         <input type="email" value={email} onChange={handleEmailInput}></input>
-//         <input type="password" value={password} onChange={handlePasswordInput}></input>
-//         <input type="button" value="submit" onClick={handleSubmit}></input>
-//       </form>
-//     </div>
-//   );
-// }
-
+export default function Login() {
+  const useStyle = makeStyles({
+    text: {
+      color: "grey",
+    },
+    card2: {
+      height: "6vh",
+      marginTop: "2%",
+    },
+  });
+  const classes = useStyle();
+  return (
+    <div className="loginWrapper">
+      <div
+        className="imgCard"
+        style={{ backgroundImage: "url(" + iphone7 + ")", background: "cover" }}
+      ></div>
+      <div className="carousel">
+        <CarouselProvider
+          naturalSlideWidth={200}
+          naturalSlideHeight={331}
+          totalSlides={3}
+          visibleSlides={1}
+          hasMasterSpinner
+          isPlaying={true}
+          infinite={true}
+          dragEnabled={false}
+          touchEnabled={false}
+        >
+          <Slider>
+            <Slide index={0}><Image className="carouselImg" src={img1}></Image></Slide>
+            <Slide index={1}><Image className="carouselImg" src={img2}></Image></Slide>
+            <Slide index={2}><Image className="carouselImg" src={img3}></Image></Slide>
+          </Slider>
+        </CarouselProvider>
+      </div>
+      <div className="signupCard">
+        <Card variant="outlined">
+          <div className="cardLogo">
+            <img src={logo} />
+          </div>
+          <CardContent>
+            <Alert severity="error" margin="dense" size="small">
+              This is an error alert — check it out!
+            </Alert>
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              fullWidth={true}
+              margin="dense"
+              size="small"
+            />
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              fullWidth={true}
+              margin="dense"
+              size="small"
+            />
+            <Typography>
+              <Link to="/Signup">Forgot Password?</Link>
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button color="primary" fullWidth="true" variant="contained">
+              LOGIN
+            </Button>
+          </CardActions>
+        </Card>
+        <Card className={classes.card2}>
+          <CardContent className="loginCard">
+            <Typography>
+              Don't have an account? <Link to="/Signup">Sign Up</Link>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
